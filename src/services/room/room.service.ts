8609105c1de03924fab940c5a4ee2480c2cd85fc
Christian Bernier <christianbernier02@gmail.com>
2023-06-25 11:49:13 -0400

    refactor room service

--- src/services/room/room.service.ts
+++ src/services/room/room.service.ts
@@ -11,31 +11,32 @@ export class RoomService {
     onError: (error: Error) => void,
     onUpdateState: (state: GameState) => void
   ): void {
+    const onMessageResponse = (event: MessageEvent): void => {
+      const eventData = JSON.parse(event.data);
+      const eventType = eventData.type;
+      const eventPayload = eventData.payload;
+
+      switch (eventType) {
+        case 'ROOM_JOINED':
+          onJoinRoom(eventPayload.initialState);
+          break;
+        case 'UPDATE_STATE':
+          onUpdateState(eventPayload.state);
+          break;
+        case 'ROOM_DOES_NOT_EXIST':
+          onError(new Error('Room does not exist.'));
+          break;
+      }
+    };
+
+    const onErrorResponse = (errorMessage: string): void => {
+      onError(new Error(`Socket error: ${errorMessage}`));
+    };
+
     this.webSocketService.initializeSocket(
       `/join?room=${roomUid}&name=${playerName}`,
-      (event: MessageEvent): void => {
-        const eventData = JSON.parse(event.data);
-        const eventType = eventData.type;
-        const eventPayload = eventData.payload;
-
-        console.log('received ', eventType, eventData);
-
-        switch (eventType) {
-          case 'ROOM_JOINED':
-            onJoinRoom(eventPayload.initialState);
-            break;
-          case 'UPDATE_STATE':
-            onUpdateState(eventPayload.state);
-            break;
-          case 'ROOM_DOES_NOT_EXIST':
-            onError(new Error('Room does not exist.'));
-            break;
-        }
-      },
-      (event: string): void => {
-        console.log(event);
-        onError(new Error('Socket error.'));
-      }
+      onMessageResponse,
+      onErrorResponse
     );
   }
 
