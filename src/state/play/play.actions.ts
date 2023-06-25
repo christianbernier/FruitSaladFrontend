7b91a2937b9ff2b96ca25fba6b0e2948659751e2
Christian Bernier <35120006+christianbernier@users.noreply.github.com>
2023-06-25 12:04:57 -0400

    Prepare Play Page for WebSockets (#10)

--- src/state/play/play.actions.ts
+++ src/state/play/play.actions.ts
@@ -1,16 +1,26 @@
 import { Card, GameState } from '../../model';
 
 export enum PlayActionType {
+  SetRoomUid = '[Play] Set Room UID',
   UpdateGameState = '[Play] Update Game State',
+  UpdateServerError = '[Play] Update Server Error',
   ClickCard = '[Play] Click Card',
 }
 
 export type PlayAction =
   | {
-      type: PlayActionType.ClickCard;
-      card: Card;
+      type: PlayActionType.SetRoomUid;
+      roomUid: string;
     }
   | {
       type: PlayActionType.UpdateGameState;
       gameState: GameState;
+    }
+  | {
+      type: PlayActionType.UpdateServerError;
+      serverError: string;
+    }
+  | {
+      type: PlayActionType.ClickCard;
+      card: Card;
     };
